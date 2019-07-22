a6b1f10360d6b0c97c56f4b708a660ceaad866c2
starsitar <prashanth@keep.network>
2019-07-22 18:00:03 +0530

    corrected dkg compromised calc

--- Mesa Simulations/agent.py
+++ Mesa Simulations/agent.py
@@ -90,8 +90,8 @@ class Group(Agent):
         if self.status == "dkg":
             if self.dkg_block_delay>=0:
                 self.dkg_block_delay -=1 # counts down the block delay
-                # based on DKG process we check for missing/malicious nodes at stage 11
-                if self.dkg_block_delay >=11:
+                # based on DKG process we check for missing/malicious nodes 4 blocks before dkg completes
+                if self.dkg_block_delay == (self.dkg_block_delay - 4):
                     self.offline_percent = self.calculate_offline()/sum(self.ownership_distr) # calculates % nodes offline during dkg
                     self.compromised_percent = self.malicious_percent #+ self.offline_percent
             else:
